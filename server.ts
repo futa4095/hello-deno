2c253c7676ff655fe7d414f62d15f1f3b1e33bd6
futa4095 <kick20000@gmail.com>
2021-10-28 12:06:25 +0900

    change message

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import { listenAndServe } from "https://deno.land/std@0.113.0/http/server.ts";
 
 function handler(_req: Request): Response {
-  return new Response("Hello world");
+  return new Response("Hello world!!!");
 }
 
 console.log("Listening on http://localhost:8000");
