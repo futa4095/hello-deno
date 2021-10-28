b37f7e32b19adec28e8505e1a8ab75387d9d8d58
futa4095 <kick20000@gmail.com>
2021-10-28 12:02:48 +0900

    Initial commit

---
+++ server.ts
@@ -0,0 +1,8 @@
+import { listenAndServe } from "https://deno.land/std@0.113.0/http/server.ts";
+
+function handler(_req: Request): Response {
+  return new Response("Hello world");
+}
+
+console.log("Listening on http://localhost:8000");
+await listenAndServe(":8000", handler);
